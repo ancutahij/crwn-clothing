e52aff3620c64274b67554e17112dc551a97e2da
Ancuta Hij <ancutahij@gmail.com>
2021-05-01 00:25:10 +0100

    refactor code by adding reselect library from memoization

--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -1,11 +1,15 @@
 import { createSelector } from "reselect";
-import cartReducer from "./cart.reducer";
 
 const selectCart = state => state.cart;
 
 export const selectCartItems = createSelector(
   [selectCart],
   cart => cart.cartItems
+);
+
+export const selectCartHidden = createSelector(
+  [selectCart],
+  cart => cart.hidden
 );
 
 export const selectCartItemsCount = createSelector(
