c6b393479663445fc97b70091fb51c0ae59bc13d
Ancuta Hij <ancutahij@gmail.com>
2021-05-06 23:26:10 +0100

    add spinner for asynchronous backend calls

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -9,11 +9,11 @@ export const selectShopItems = createSelector(
 
 export const selectCollectionsForPreview = createSelector(
   [selectShopItems],
-  collections => Object.keys(collections).map(key => collections[key])
+  collections =>
+    collections ? Object.keys(collections).map(key => collections[key]) : []
 );
 
 export const selectCollection = collectionUrlParam =>
-  createSelector(
-    [selectShopItems],
-    collections => collections[collectionUrlParam]
+  createSelector([selectShopItems], collections =>
+    collections ? collections[collectionUrlParam] : null
   );
